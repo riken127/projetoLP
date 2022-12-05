a8fa5aa5974611da7dc91e3d393948b72b2b2641
NatanFC22 <joaopereira237@gmail.com>
2022-12-05 16:30:40 +0000

    novoMain
    novoMain

--- projetoFinal/main.c
+++ projetoFinal/main.c
@@ -17,61 +17,66 @@ int menuRead(char message[], int min, int max) {
     } while (option < min || option > max);
     return option;
 }
-int verifyCustomersId(int customer[], int contCustomers, int requestedId)
-{
-    int i = 0, count = 0;
-    for (i = 0; i < contCustomers; i++)
-        {
-        if (customer[i] == requestedId)
-            {
-                ++count;
-                break;
-            }
-        }
 
+int verifyCustomersId(int customer[], int contCustomers, int requestedId) {
+    int i = 0, count = 0;
+    for (i = 0; i < contCustomers; i++) {
+        if (customer[i] == requestedId) {
+            ++count;
+            break;
+        }
+    }
     return count;
 }
 
 int registCustomers(int customer[], int contCustomers) {
     int id, verify;
-    printf("Regist - ");
-    scanf("%d", &id);
-    verify = verifyCustomersId(customer, contCustomers, id);
-    if (verify == 1)
-        printf("The given id is already taken.\n");
-    else
-        customer[contCustomers] = id;
-
+    do {
+        printf("Regist - ");
+        scanf("%d", &id);
+        verify = verifyCustomersId(customer, contCustomers, id);
+        if (verify == 1)
+            printf("The given id is already taken.\n");
+        else
+            customer[contCustomers] = id;
+    } while (verify != 0);
     return contCustomers + 1;
 }
 
 void editCustomers(int customer[], int contCustomers) {
     int id, i, verify;
-    do{
-    printf("Edit - ");
-    scanf("%d", &id);
+    do {
+        printf("Edit - ");
+        scanf("%d", &id);
 
-    verify = verifyCustomersId(customer, contCustomers, id);
-    if (verify == 1) {
-        for (i = 0; i < contCustomers; i++) {
-            if (customer[i] == id) {
-                printf("Change %d's id:\n", id);
-                scanf(" %d", &customer[i]);
+        verify = verifyCustomersId(customer, contCustomers, id);
+        if (verify == 1) {
+            for (i = 0; i < contCustomers; i++) {
+                if (customer[i] == id) {
+                    printf("Change %d's id:\n", id);
+                    scanf(" %d", &customer[i]);
+                }
             }
-        }
-    } else printf("");
-    }while(verify != 1);
+        } else printf("");
+    } while (verify != 1);
 }
 
 int deleteCustomers(int customer[], int contCustomers) {
-    int id, i;
-    printf("Delete - ");
-    scanf("%d", &id);
-    for (i = 0; i < contCustomers; i++) {
-        if (customer[i] == id) {
-            customer[i] = customer[i + 1];
+    int id, i, verify;
+    do {
+        printf("Delete - ");
+        scanf("%d", &id);
+        verify = verifyCustomersId(customer, contCustomers, id);
+        if (verify == 0)
+            printf("The given id does not exist.\n");
+        else {
+            for (i = 0; i < contCustomers; i++) {
+                if (customer[i] == id) {
+                    customer[i] = customer[i + 1];
+                }
+            }
         }
-    }
+    } while (verify != 1);
     return contCustomers - 1;
 }
 
