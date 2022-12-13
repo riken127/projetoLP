ed658f9c8f309381d5a99491bc174396145886bb
NatanFC22 <joaopereira237@gmail.com>
2022-12-13 10:20:42 +0000

    pk o henrique pediu com jeitinho
    pk o henrique pediu com jeitinho

--- projetoFinal/products.c
+++ projetoFinal/products.c
@@ -9,19 +9,32 @@
 
 void productsManagementMenu(){
     int option;
-    Materials material[10];
+    Materials material[11];
 
-    material[0] = {1, "Tubo de Cola"};
-    material[1] = {2, "Base"};
-    material[2] = {3, "Parafuso"};
-    material[3] = {4, "Bucha"};
-    material[4] = {5, "Pés"};
-    meterial[5] = {6, "Porta"};
-    material[6] = {7, "Laterais"};
-    material[7] = {8, "Cavilhas"};
-    material[8] = {9, "Prateleiras"};
-    material[9] = {10, "Ferragem"};
-    material[10] = {11, "Frente"};
+    material[0].cod_Material = 1;
+    material[1].cod_Material = 2;
+    material[2].cod_Material = 3;
+    material[3].cod_Material = 4;
+    material[4].cod_Material = 5;
+    material[5].cod_Material = 6;
+    material[6].cod_Material = 7;
+    material[7].cod_Material = 8;
+    material[8].cod_Material = 9;
+    material[9].cod_Material = 10;
+    material[10].cod_Material = 11;
+    
+    strcpy(material[0].description, "Tubo de Cola");
+    strcpy(material[1].description, "Base");
+    strcpy(material[2].description, "Parafuso");
+    strcpy(material[3].description, "Bucha");
+    strcpy(material[4].description, "Pés");
+    strcpy(material[5].description, "Porta");
+    strcpy(material[6].description, "Laterais");
+    strcpy(material[7].description, "Cavilhas");
+    strcpy(material[8].description, "Prateleiras");
+    strcpy(material[9].description, "Ferragem");
+    strcpy(material[10].description, "Frente");
+    
     do {
         option = menuRead(MSG_PRODUCTS_MANAGEMENT_MENU, 0, 4);
 
