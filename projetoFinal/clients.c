29ace3bbb73b0bc921ad55cb3228ee618d029cd7
NatanFC22 <joaopereira237@gmail.com>
2023-01-03 12:36:36 +0000

    mais
    mais

--- projetoFinal/clients.c
+++ projetoFinal/clients.c
@@ -8,14 +8,14 @@
 #define MSG_CUSTOMER_MANAGEMENT_MENU                                           \
   "\n\t\t\t========= Customer Managment Menu =========\n\n\t\t\t[1] - Record.\n\t\t\t[2] - Edit.\n\t\t\t[3] - Delete.\n\t\t\t[4] - List.\n\t\t\t[0] - Quit.\n\t\t\t___________________________________________"
 #define MSG_CHANGE_CUSTOMER_DATA                                               \
-  "\n\t\t\t========= Edit =========\n\n\t\t\t[1] - Name\n\t\t\t[2] - Adress\n\t\t\t[3] - Nif\n\t\t\t[4] - Country.\n\t\t\t________________________"
-#define MSG_CUSTOMER_NAME "Name - "
-#define MSG_CUSTOMER_ADDRESS "Adress - "
-#define MSG_CUSTOMER_NIF "Nif - "
-#define MSG_CUSTOMER_COUNTRY "Country - "
+  "\n\t\t\t========= Edit =========\n\n\t\t\t[1] - Name\n\t\t\t[2] - Adress\n\t\t\t[3] - Nif\n\t\t\t[4] - Country.\n\t\t\t[0] - Quit.\n\t\t\t________________________"
+#define MSG_CUSTOMER_NAME "\t\t\tName - "
+#define MSG_CUSTOMER_ADDRESS "\t\t\tAdress - "
+#define MSG_CUSTOMER_NIF "\t\t\tNif - "
+#define MSG_CUSTOMER_COUNTRY "\t\t\tCountry - "
 #define CLIENT_ID_MSG "\n\t\t\tType the desired client id - "
 #define MSG_ERROR_MESSAGE "\n\t\t\t========== ERROR MESSAGE ==========\n\n\t\t\tThe following option does not exist\n\t\t\t___________________________________"
-
+#define YES_OR_NO_MESSAGE "\t\t\tDo you want to add another record?[y/n] - "
 
 int menuRead(char message[], int min, int max) {
     int option;
@@ -39,6 +39,22 @@ int menuRead(char message[], int min, int max) {
  * is between to values, if not, the cycle continues.
  */
 
+int yesOrNoFunction(){
+    char option;
+    do{
+        printf(YES_OR_NO_MESSAGE);
+        scanf(" %c", &option);
+    }while(option != 'y' && option != 'n');
+    switch(option){
+        case 'y':
+            return 1;
+            break;
+        case 'n':
+            return 2;
+            break;
+    }
+}
+
 int verifyCustomersId(Customers *customer, int requestedId) {
     int i = 0, count = 0;
     for (i = 0; i < customer->counter; i++) {
@@ -56,7 +72,7 @@ int verifyCustomersId(Customers *customer, int requestedId) {
 
 void customerName(char name[]) {
     char temp;
-    printf(MSG_CUSTOMER_NAME);
+    printf("\n"MSG_CUSTOMER_NAME);
     scanf("%c", &temp);
     scanf("%[^\n]", name);
 }
@@ -69,7 +85,7 @@ void customerName(char name[]) {
 
 void customerAddress(char address[]) {
     char temp;
-    printf(MSG_CUSTOMER_ADDRESS);
+    printf("\n"MSG_CUSTOMER_ADDRESS);
     scanf("%c", &temp);
     scanf("%[^\n]", address);
 }
@@ -82,7 +98,7 @@ void customerAddress(char address[]) {
 int customerNif() {
     int verifyNif, nif;
     do {
-        printf(MSG_CUSTOMER_NIF);
+        printf("\n"MSG_CUSTOMER_NIF);
         scanf("%d", &nif);
         if (nif <= 0) {
             printf("Invalid Nif\n");
@@ -99,7 +115,7 @@ int customerNif() {
  */
 void customerCountry(char country[]) {
     char temp;
-    printf(MSG_CUSTOMER_COUNTRY);
+    printf("\n"MSG_CUSTOMER_COUNTRY);
     scanf("%c", &temp);
     scanf("%[^\n]", country);
 }
@@ -117,25 +133,29 @@ int customerId(int curentID) { return (curentID + 1); }
  */
 void saveCustomer(char name[], char address[], int nif, char country[], int id,
                   Customers *customer, int pos) {
-    // currentID++;
     *(&customer->customers[pos].id) = id;
     strcpy(*(&customer->customers[pos].name), name);
     strcpy(*(&customer->customers[pos].address), address);
     *(&customer->customers[pos].nif) = nif;
     strcpy(*(&customer->customers[pos].country), country);
 }
-void recordCustomers(Customers *customer /*, int curentID*/) {
+void recordCustomers(Customers *customer) {
     char name[MAX_NAME_CHARS], address[MAX_ADDRESS_CHARS],
             country[MAX_COUNTRY_CHARS];
-    int nif;
-    customerName(name);
-    customerAddress(address);
-    nif = customerNif();
-    customerCountry(country);
-    saveCustomer(name, address, nif, country, customerId(customer->counter),
+    int nif, option = 1;
+    do{
+        printf("\t\t\t__________________________________\n");
+        customerName(name);
+        customerAddress(address);
+        nif = customerNif();
+        customerCountry(country);
+        printf("\t\t\t__________________________________\n");
+        saveCustomer(name, address, nif, country, customerId(customer->counter),
                  *(&customer), customer->counter);
-
-    customer->counter++;
+        customer->counter++;
+        option = yesOrNoFunction();
+        system("cls || clear");
+    }while(option != 2);
 }
 void changeCustomerData(Customers *customer, int pos, int id) {
     int option, nif;
@@ -152,16 +172,24 @@ void changeCustomerData(Customers *customer, int pos, int id) {
             case 0:
                 break;
             case 1:
+                printf("\n\t\t\tPrevious - %s",name);
                 customerName(name);
+                system("cls || clear");
                 break;
             case 2:
+                printf("\n\t\t\tPrevious - %s",address);
                 customerAddress(address);
+                system("cls || clear");
                 break;
             case 3:
+                printf("\n\t\t\tPrevious - %d",nif);
                 nif = customerNif();
+                system("cls || clear");
                 break;
             case 4:
+                printf("\n\t\t\tPrevious - %s",country);
                 customerCountry(country);
+                system("cls || clear");
                 break;
         }
     } while (option != 0);
@@ -200,7 +228,7 @@ void editCustomers(Customers *customer) {
 int deleteCustomers(Customers *customer) {
     int id, i, verify;
     do {
-        printf("Delete - ");
+        printf(CLIENT_ID_MSG);
         scanf("%d", &id);
         if (id == 0) {
             break;
@@ -224,7 +252,7 @@ int deleteCustomers(Customers *customer) {
             }
         }
     } while (verify != 1);
-
+    system("cls || clear");
     if (id == 0)
         return customer->counter;
     else
@@ -239,20 +267,24 @@ int deleteCustomers(Customers *customer) {
  */
 
 void listCustomers(Customers *customer) {
-    int i;
-    printf("\n");
-    printf("%d\n", customer->counter);
+    int i,any_key[20];
+    printf("\n\t\t\tList Of Clients\n\t\t\t__________________________________");
     for (i = 0; i < customer->counter; i++) {
-        printf("| %d | %s | %s | %d | %s |\n", customer->customers[i].id,
-               customer->customers[i].name, customer->customers[i].address,
-               customer->customers[i].nif, customer->customers[i].country);
+        printf("\n\n\t\t\tClient Id : %d",customer->customers[i].id);
+        printf("\n\t\t\tName      : %s",customer->customers[i].name);
+        printf("\n\t\t\tAddress   : %s",customer->customers[i].address);
+        printf("\n\t\t\tNif       : %d",customer->customers[i].nif);
+        printf("\n\t\t\tCountry   : %s",customer->customers[i].country);
+        printf("\n\t\t\t__________________________________");
     }
-    printf("\n");
+    printf("\n\t\t\tPress any key to exit ");
+    scanf("%s",any_key);
+    system("cls || clear");
 }
 /*
  * The above function displays all customers.
  */
-void customerManagementMenu(Customers *customer /*, int curentID*/) {
+void customerManagementMenu(Customers *customer) {
     int option;
 
     do {
@@ -262,8 +294,7 @@ void customerManagementMenu(Customers *customer /*, int curentID*/) {
             case 0:
                 break;
             case 1:
-                recordCustomers(*(&customer) /*, curentID*/);
-                //++customer->counter;
+                recordCustomers(*(&customer));
                 break;
             case 2:
                 editCustomers(customer);
@@ -271,7 +302,7 @@ void customerManagementMenu(Customers *customer /*, int curentID*/) {
             case 3:
                 deleteCustomers(customer);
                 break;
-            case 4:
+            case 4:        
                 listCustomers(customer);
                 break;
         }
