1ac1a06d7baa3cb8f62f286937f66a40cc2945b2
NatanFC22 <joaopereira237@gmail.com>
2023-01-04 09:33:38 +0000

    piavense fdp
    paivense burro

--- projetoFinal/clients.c
+++ projetoFinal/clients.c
@@ -8,18 +8,6 @@
 #include <string.h>
 #include <stdlib.h>
 
-#define MSG_CUSTOMER_MANAGEMENT_MENU                                           \
-  "\n\t\t\t========= Customer Managment Menu =========\n\n\t\t\t[1] - Record.\n\t\t\t[2] - Edit.\n\t\t\t[3] - Delete.\n\t\t\t[4] - List.\n\t\t\t[0] - Quit.\n\t\t\t___________________________________________"
-#define MSG_CHANGE_CUSTOMER_DATA "\n\t\t\t========= Edit =========\n\n\t\t\t[1] - Name\n\t\t\t[2] - Adress\n\t\t\t[3] - Nif\n\t\t\t[4] - Country.\n\t\t\t[0] - Quit.\n\t\t\t________________________"
-#define MSG_CUSTOMER_NAME "\t\t\tName - "
-#define MSG_CUSTOMER_ADDRESS "\t\t\tAddress - "
-#define MSG_CUSTOMER_NIF "\t\t\tNif - "
-#define MSG_CUSTOMER_COUNTRY "\t\t\tCountry - "
-#define CLIENT_ID_MSG "\n\t\t\tType the desired client id - "
-#define MSG_ERROR_MESSAGE "\n\t\t\t========== ERROR MESSAGE ==========\n\n\t\t\tThe following option does not exist\n\t\t\t___________________________________"
-#define YES_OR_NO_MESSAGE "\t\t\tDo you want to add another record?[y/n] - "
-#define SUCCESS_IN_WRITING_CUSTOMERS "\nCustomer were written successfully"
-#define ERROR_IN_WRITING_CUSTOMERS "\nAn error has occured"
 
 int menuRead(char message[], int min, int max) {
     int option;
