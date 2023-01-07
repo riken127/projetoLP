ce6198ecac866b44f7d4d18e9605ae2b990a51aa
riken <henriquenoronha05@gmail.com>
2023-01-07 18:08:30 +0000

    300 palavras por segundo meus meninos

--- projetoFinal/products.c
+++ projetoFinal/products.c
@@ -10,7 +10,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MSG_PRODUCTS_MANAGEMENT_MENU "[1] - Save.\n[2] - Load.\n[3] - Delete.\n"
 
 void fillStruct(Products *product) {
     strcpy((product->product[0].cod_Produto), "P00001");
@@ -52,19 +51,93 @@ void fillStruct(Products *product) {
     product->counter = 2;
 }
 
-void saveProducts(Products *products){
+void productCode(char productCode[COD_PRODUCT_SIZE]) {
+    char temp;
+    printf(ASK_PRODUCT_CODE);
+    scanf("%c", &temp);
+    scanf("%[^\n]", productCode);
+}
+
+void productName(char productName[MAX_PRODUCT_NAME_SIZE]) {
+    char temp;
+    printf(ASK_PRODUCT_NAME);
+    scanf("%c", &temp);
+    scanf("%[^\n]", productName);
+}
+
+void productDimension(Dimensions *productDimension) {
+    printf(ASK_PRODUCT_DIMENSION);
+    scanf("%dx%dx%d", &productDimension->lenght,
+          &productDimension->height,
+          &productDimension->width);
+}
+
+void productPrice(double *price) {
+    printf(ASK_PRODUCT_PRICE);
+    scanf("%lf", price);
+}
+
+void materialCode(char materialCode[COD_MATERIAL_SIZE]) {
+    char temp;
+    printf(ASK_MATERIAL_CODE);
+    scanf("%c", &temp);
+    scanf("%[^\n]", materialCode);
+}
+
+void materialDescription(char materialDescription[MAX_DESCRIPTION_SIZE]) {
+    char temp;
+    printf(ASK_MATERIAL_DESCRIPTION);
+    scanf("%c", &temp);
+    scanf("%[^\n]", materialDescription);
+}
+
+void materialQuantity(short int *materialQuantity) {
+    printf(ASK_MATERIAL_QUANTITY);
+    scanf("%hu", *(&materialQuantity));
+}
+
+void materialUnit(units *materialUnit) {
+    printf(ASK_MATERIAL_UNIT);
+    scanf("%u", *(&materialUnit));
+}
+
+
+void newProduct(Products *product) {
+    int i;
+    bool op;
+    product->product = (Product *) realloc(product->product, sizeof(Product) * (++product->counter));
+    product->product[product->counter - 1].material_count = 1;
+    product->product[product->counter - 1].material = (Materials *)malloc(sizeof(Materials) * 1);
+    productCode(product->product[product->counter - 1].cod_Produto);
+    productName(product->product[product->counter - 1].name);
+    productDimension(&(product->product[product->counter - 1].dimension));
+    productPrice(&product->product[product->counter - 1].price);
+    i = 0;
+    do {
+        materialCode(product->product[product->counter - 1].material[i].cod_Material);
+        materialDescription(product->product[product->counter - 1].material[i].description);
+        materialQuantity(&product->product[product->counter - 1].material[i].quantity);
+        materialUnit(&product->product[product->counter - 1].material[i].unit);
+        op = menuRead(ASK_ANOTHER_MATERIAL, 0, 1);
+        i++;
+
+        product->product[product->counter - 1].material = (Materials *)realloc(product->product[product->counter - 1].material, sizeof(Materials) * (i + 1));
+    } while (op != false);
+    product->product[product->counter - 1].material_count = (i);
+}
+
+void saveProducts(Products *products) {
     FILE *fp;
     int i, j, c;
     char fn[MAX_FN_CHARS];
     askFileName(fn);
     fp = fopen(fn, "w+");
-    if (fp == NULL){
+    if (fp == NULL) {
         exit(EXIT_FAILURE);
     }
-    //printf("counter:\t%d\n", products->counter);
     fwrite(&products->counter, sizeof(int), 1, fp);
     i = 0;
-    do{
+    do {
         fwrite(&products->product[i].cod_Produto, sizeof(char) * COD_PRODUCT_SIZE, 1, fp);
         fwrite(&products->product[i].name, sizeof(char) * MAX_PRODUCT_NAME_SIZE, 1, fp);
         fwrite(&products->product[i].price, sizeof(double), 1, fp);
@@ -72,60 +145,84 @@ void saveProducts(Products *products){
         fwrite(&products->product[i].dimension.lenght, sizeof(int), 1, fp);
         fwrite(&products->product[i].dimension.width, sizeof(int), 1, fp);
         i++;
-    }while(i < products->counter);
+    } while (i < products->counter);
 
-    for (i = 0; i < products->counter; i++){
+    for (i = 0; i < products->counter; i++) {
         j = 0;
         fwrite(&products->product[i].material_count, sizeof(int), 1, fp);
         printf("material:\t%d\n", products->product[i].material_count);
-        do{
-            fwrite(&products->product[i].material[j].cod_Material, sizeof(char)*COD_MATERIAL_SIZE, 1, fp);
-            fwrite(&products->product[i].material[j].description, sizeof(char)*MAX_DESCRIPTION_SIZE, 1, fp);
+        do {
+            fwrite(&products->product[i].material[j].cod_Material, sizeof(char) * COD_MATERIAL_SIZE, 1, fp);
+            fwrite(&products->product[i].material[j].description, sizeof(char) * MAX_DESCRIPTION_SIZE, 1, fp);
             fwrite(&products->product[i].material[j].quantity, sizeof(short int), 1, fp);
             fwrite(&products->product[i].material[j].unit, sizeof(units), 1, fp);
             j++;
-        }while(j < products->product[i].material_count);
+        } while (j < products->product[i].material_count);
     }
     printf("%d", c);
     fclose(fp);
     printf(SUCCESS_IN_WRITING_ORDERS);
 }
-void loadProducts(Products *products){
+
+void loadProducts(Products *products) {
     FILE *fp;
-    int i, j, c;
+    int i, j, c, k;
     char fn[MAX_FN_CHARS], buff[1024], *sp, *d;
     askFileName(fn);
     fp = fopen(fn, "rb+");
     fread(&c, sizeof(int), 1, fp);
-    products->product = (Product *)realloc(products->product, (c + products->counter) * sizeof(Product));
-    for (i = 0; i < c; i++){
-        //fread(&products->product[i], sizeof(Products), 1, fp);
-        fread(&products->product[i + products->counter].cod_Produto, sizeof(char) * COD_PRODUCT_SIZE,1,fp);
-        fread(&products->product[i + products->counter].name, sizeof(char)*MAX_PRODUCT_NAME_SIZE, 1, fp);
-        fread(&products->product[i + products->counter].price, sizeof(double), 1, fp);
-        fread(&products->product[i + products->counter].dimension.height, sizeof(int), 1, fp);
-        fread(&products->product[i + products->counter].dimension.lenght, sizeof(int), 1, fp);
-        fread(&products->product[i + products->counter].dimension.width, sizeof(int), 1, fp);
+    k = products->counter;
+    for (i = 0; i < c; i++) {
+        products->product = (Product *) realloc(products->product, (products->counter + 1) * sizeof(Product));
+        fread(&products->product[products->counter].cod_Produto, sizeof(char) * COD_PRODUCT_SIZE, 1, fp);
+        fread(&products->product[products->counter].name, sizeof(char) * MAX_PRODUCT_NAME_SIZE, 1, fp);
+        fread(&products->product[products->counter].price, sizeof(double), 1, fp);
+        fread(&products->product[products->counter].dimension.height, sizeof(int), 1, fp);
+        fread(&products->product[products->counter].dimension.lenght, sizeof(int), 1, fp);
+        fread(&products->product[products->counter].dimension.width, sizeof(int), 1, fp);
+        products->counter++;
     }
-    products->counter += i;
-    for(i = 0; i < c;i++){
-        fread(&products->product[i].material_count, sizeof(int), 1, fp);
-        products->product[i].material = (Materials *)malloc(sizeof(Materials)*products->product[i].material_count);
-        printf("material_count: %d\n", products->product[i].material_count);
-        for (j = 0; j < products->product[i].material_count; j++){
-            fread(&products->product[i].material[j].cod_Material, sizeof(char) * COD_MATERIAL_SIZE, 1, fp);
-            fread(&products->product[i].material[j].description, sizeof(char)*MAX_DESCRIPTION_SIZE, 1, fp);
-            fread(&products->product[i].material[j].quantity, sizeof(short int), 1, fp);
-            fread(&products->product[i].material[j].unit, sizeof(int), 1, fp);
+    printf("%d", products->counter);
+    for (i = 0; i < c; i++) {
+        fread(&products->product[k + i].material_count, sizeof(int), 1, fp);
+        printf("%d", products->product[k + i].material_count);
+        products->product[k + i].material = (Materials *) malloc(sizeof(Materials) * products->product[k + i].material_count);
+        printf("material_count: %d\n", products->product[k + i].material_count);
+        for (j = 0; j < products->product[k + i].material_count; j++) {
+            fread(&products->product[k + i].material[j].cod_Material, sizeof(char) * COD_MATERIAL_SIZE, 1, fp);
+            fread(&products->product[k + i].material[j].description, sizeof(char) * MAX_DESCRIPTION_SIZE, 1, fp);
+            fread(&products->product[k + i].material[j].quantity, sizeof(short int), 1, fp);
+            fread(&products->product[k + i].material[j].unit, sizeof(int), 1, fp);
         }
-        products->product[i].material_count += j;
+        products->product[k + i].material_count = (j);
     }
-    printf("%s",products->product[1].material[0].cod_Material);
+
     fclose(fp);
 }
+
 /*
  * carregar por memória já que ainda n sabemos como fazer por ficheiro
  */
+void listProducts(Products *product){
+    int i, j;
+    printf("Product list.\n");
+    for (i = 0; i < product->counter; i++){
+        printf("%s, %s, %.2lf, %dx%dx%d\n",
+               product->product[i].cod_Produto,
+               product->product[i].name,
+               product->product[i].price,
+               product->product[i].dimension.height,
+               product->product[i].dimension.width,
+               product->product[i].dimension.lenght);
+        for (j = 0; j < product->product[i].material_count; j++){
+            printf("\t%s, %s, %hu, %d\n",
+                   product->product[i].material[j].cod_Material,
+                   product->product[i].material[j].description,
+                   product->product[i].material[j].quantity,
+                   product->product[i].material[j].unit);
+        }
+    }
+}
 
 void productsManagementMenu(Products *products) {
     int option;
@@ -135,9 +232,9 @@ void productsManagementMenu(Products *products) {
         products->product[i].material = malloc(2 * sizeof(Materials));
     }
 
-    //fillStruct(products);
+
     do {
-        option = menuRead(MSG_PRODUCTS_MANAGEMENT_MENU, 0, 2);
+        option = menuRead(MSG_PRODUCTS_MANAGEMENT_MENU, 0, 6);
 
         switch (option) {
             case 0:
@@ -149,6 +246,16 @@ void productsManagementMenu(Products *products) {
                 loadProducts(products);
                 break;
             case 3:
+                newProduct(products);
+                break;
+            case 4:
+                //edit.
+                break;
+            case 5:
+                //delete.
+                break;
+            case 6:
+                listProducts(products);
                 break;
         }
     } while (option != 0);
--- projetoFinal/products.h
+++ projetoFinal/products.h
@@ -4,15 +4,45 @@
 
 #ifndef PROJETO_PRODUCTS_H
 #define PROJETO_PRODUCTS_H
+#include <stdbool.h>
 
 #define MAX_MATERIALS 20
 #define MAX_QUANTITY 20
+
+#define MSG_PRODUCTS_MANAGEMENT_MENU "[1] - Save.\n[2] - Load.\n[3] - Create.\n[4] - Edit.\n[5] - Delete.\n[6] - List.\n"
 
 #define COD_MATERIAL_SIZE 6
 #define COD_PRODUCT_SIZE 7
 
 #define MAX_DESCRIPTION_SIZE 100
 #define MAX_PRODUCT_NAME_SIZE 100
+
+#define ASK_PRODUCT_CODE "Product's code - "
+
+#define ASK_PRODUCT_NAME "Product's name - "
+
+#define ASK_PRODUCT_DIMENSION "Product's dimension (#x#x#) - "
+
+#define ASK_PRODUCT_PRICE "Product's price - "
+
+#define ASK_MATERIAL_CODE "Material's code - "
+
+#define ASK_MATERIAL_DESCRIPTION "Material's description - "
+
+#define ASK_MATERIAL_QUANTITY "Material's quantity - "
+
+#define ASK_MATERIAL_UNIT "Material's unit - "
+
+#define ASK_ANOTHER_MATERIAL "Would you like to type another material?[1/0]\n"
+
+#define INITIAL_PRODUCT_SIZE 1
+#define INITIAL_MATERIAL_SIZE 1
+
+#define ERROR_READING "ERROR READING FROM FILE\n"
+#define SUCCESS_READING "DATA READ OK.\n"
+
+#define ERROR_WRITING "ERROR WRITING IN FILE\n"
+#define SUCCESS_WRITING "WRITE DATA OK.\n"
 typedef enum {UN, PAR} units;
 
 typedef struct {
@@ -36,6 +66,18 @@ typedef struct {
     Product *product;
     int counter;
 }Products;
+void fillStruct(Products *product);
+void productName(char productCode[COD_PRODUCT_SIZE]);
+void productName(char productName[MAX_PRODUCT_NAME_SIZE]);
+void productDimension(Dimensions *productDimension);
+void productPrice(double *price);
+void materialCode(char materialCode[COD_MATERIAL_SIZE]);
+void materialDescription(char materialDescription[MAX_DESCRIPTION_SIZE]);
+void materialQuantity(short int *materialQuantity);
+void materialUnit(units *materialUnit);
+bool newMaterial();
+void newProduct(Products *product);
+void listProducts(Products *product);
 int menuRead(char message[], int min, int max);
 void productsManagementMenu(Products *products);
 #endif //PROJETO_PRODUCTS_H
