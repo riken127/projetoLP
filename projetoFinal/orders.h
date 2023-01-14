2ab0a2365ae986b0672ed89a0048c63c49faf87c
riken <henriquenoronha05@gmail.com>
2023-01-14 16:18:23 +0000

    comentários no orders.h

--- projetoFinal/orders.h
+++ projetoFinal/orders.h
@@ -5,37 +5,48 @@
 #include "clients.h"
 #include "products.h"
 
-#define ASK_PRODUCT_ID "\nPlease type the desired id:"
-#define ASK_ORDER_DATE "Please type the desired date:"
-#define ASK_ORDER_QUANTITY "Please type the desired quantity:"
+//macro definition
+#define ASK_ORDER_DATE "\n\t\t\tPlease type the desired date:"
+
+#define ASK_ORDER_QUANTITY "\n\t\t\tPlease type the desired quantity:"
 
 #define MIN_DAY 0
+
 #define MAX_DAY 31
 
 #define MIN_MONTH 0
+
 #define MAX_MONTH 12
 
 #define MIN_YEAR 2022
 
 #define MIN_ORDER_QUANTITY 0
+
 #define MAX_ORDER_QUANTITY 10000
 
 #define NIF_MAX_SIZE 999999999
+
 #define NIF_MIN_SIZE 000000000
 
 #define ASK_FILE_NAME "\n\t\t\tPlease type the desired file name - "
+
 #define MAX_FN_CHARS 100
 
-#define ASK_CUSTOMER_NIF "Please type your nif"
+#define ASK_CUSTOMER_NIF "\n\t\t\tPlease type your nif"
+
 #define NIF_ERROR_MSG "Typed nif does not exist!"
 
-#define SUCCESS_IN_WRITING_ORDERS "\nOrders where written successfully!"
+#define SUCCESS_IN_WRITING_ORDERS "\n\t\t\tOrders where written successfully!"
+
 #define ERROR_IN_WRITING_ORDERS "\nAn error has occured while writing the orders! :/"
 
 #define SUCCESS_IN_LOADING_ORDERS "\nOrders where loaded successfully!"
+
 #define ERROR_IN_LOADING_ORDERS "\nAn error has occures while loading the orders! :/"
 
-#define ASK_ANOTHER_PRODUCT "\nDo wish to add another product?[1/0]"
+#define ASK_ANOTHER_PRODUCT "\n\t\t\tDo wish to add another product?[1/0]"
+
+//struct definition
 typedef struct {
     int day, month, year;
 } Date;
@@ -57,8 +68,9 @@ typedef struct {
     int counter;
 } Orders;
 
+//function definition
 void greetCustomer(Customers *customer, int nif);
-void listAvaibleProducts(Products *product);
+void listAvailableProducts(Products *product);
 int newOrderId(Orders *order);
 int newOrderCustomerId(Customers *customer, int nif);
 void newOrderChoosenProductCode(Products *product, char code[COD_PRODUCT_SIZE]);
