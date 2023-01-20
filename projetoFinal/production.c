86ae1db7a8c9ce2acf48c51dd16752df1317379b
NatanFC22 <joaopereira237@gmail.com>
2023-01-20 16:25:14 +0000

    cr7
    cr7

--- projetoFinal/production.c
+++ projetoFinal/production.c
@@ -178,7 +178,7 @@ int listMenu(Materials *material, Orders *order, Products *product, Date date) {
                 quantityPerOrder();
                 break;
         }
-    return option;
+        return option;
 }
 
 /*
@@ -192,7 +192,8 @@ int listMenu(Materials *material, Orders *order, Products *product, Date date) {
  */
 void listMaterials(Materials *material, Orders *order, Products *product) {
     Date date;
-    int i, j, k, d, f, option = 1;
+    int i, j, k, d, f,verify = 0, option = 1;
+    do{
     if (order->counter != 0) {
         date = askDate();
         struct tm t = {.tm_year = date.year, .tm_mon = date.month, .tm_mday = date.day};
@@ -202,7 +203,6 @@ void listMaterials(Materials *material, Orders *order, Products *product) {
         for (i = 0; i < order->counter; i++) {
             if (order->order[i].order_date.day >= date.day && order->order[i].order_date.month >= date.month && order->order[i].order_date.year >= date.year ||
                     order->order[i].order_date.day <= t.tm_mday && order->order[i].order_date.month <= t.tm_mon && order->order[i].order_date.year <= t.tm_year) {
-                do{
                 for (j = 0; j < order->order[i].line_order_size; j++) {
                     for (k = 0; k < product->counter; k++) {
                         if (strcmp(order->order[i].line_order[j].code, product->product[k].cod_Produto) == 0) {
@@ -220,17 +220,19 @@ void listMaterials(Materials *material, Orders *order, Products *product) {
                         }
                     }
                 }
-                option = listMenu(*(&material), *(&order), *(&product), date);
-                system("cls || clear");
-                printf("\n\t\t\tList Of Materials for %d-%d-%d\n\t\t\t__________________________________", date.day, date.month, date.year);
-                }while (option != 0);
             } else {
                 errorMessage(NO_ORDERS_FOUND_TO_THAT_DATE_MESSAGE);
+                verify = 1;
             }
         }
     } else {
         errorMessage(NO_ORDERS_FOUND_MESSAGE);
+        verify = 1;
     }
+    if(verify==0){
+        option = listMenu(*(&material), *(&order), *(&product), date);
+    }
+    }while(option!=0);
 }
 
 /*
