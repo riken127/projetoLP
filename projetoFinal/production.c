ddd2c935672d820929886f1b6270bc4d2224fa1b
riken <henriquenoronha05@gmail.com>
2023-01-22 15:08:25 +0000

    fix datas production

--- projetoFinal/production.c
+++ projetoFinal/production.c
@@ -313,7 +313,7 @@ int listMenu(Materials *material, Orders *order, Products *product, Date date, C
  */
 void listMaterials(Materials *material, Orders *order, Products *product, Customers *customer) {
     Date date;
-    int i, j, k, d, f, verify = 0, option = 1;
+    int i, j, k, d, f, verify = 0, option = 1, count = 0;
 
     if (order->counter != 0) {
         date = askDate();
@@ -342,11 +342,14 @@ void listMaterials(Materials *material, Orders *order, Products *product, Custom
                             }
                         }
                     }
-                } else {
-                    errorMessage(NO_ORDERS_FOUND_TO_THAT_DATE_MESSAGE);
-                    verify = 1;
+                    ++count;
                 }
             }
+            if (count == 0){
+                errorMessage(NO_ORDERS_FOUND_TO_THAT_DATE_MESSAGE);
+                verify = 1;
+            }
+
             if (verify == 0) {
                 option = listMenu(*(&material), *(&order), *(&product), date, *(&customer));
             }
