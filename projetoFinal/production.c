a516b7ad785eb6a6c93a62f8191398ef001df5cd
riken <henriquenoronha05@gmail.com>
2023-01-22 15:49:42 +0000

    fix datas production

--- projetoFinal/production.c
+++ projetoFinal/production.c
@@ -326,7 +326,7 @@ void listMaterials(Materials *material, Orders *order, Products *product, Custom
         do {
             printf("\n\t\t\tList Of Materials for %d-%d-%d\n\t\t\t__________________________________", date.day, date.month, date.year);
             for (i = 0; i < order->counter; i++) {
-                if (order->order[i].order_date.day >= date.day && order->order[i].order_date.month >= date.month && order->order[i].order_date.year >= date.year ||
+                if (order->order[i].order_date.day >= date.day && order->order[i].order_date.month >= date.month && order->order[i].order_date.year >= date.year &&
                         order->order[i].order_date.day <= t.tm_mday && order->order[i].order_date.month <= t.tm_mon && order->order[i].order_date.year <= t.tm_year) {
                     for (j = 0; j < order->order[i].line_order_size; j++) {
                         for (k = 0; k < product->counter; k++) {
